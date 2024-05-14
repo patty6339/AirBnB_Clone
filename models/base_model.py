b5c00b10b7794a755c8b419c1850cec6b5b26eb0
Patrick <patty6339@gmail.com>
2024-05-14 22:59:43 +0300

    main files

---
+++ models/base_model.py
@@ -0,0 +1,75 @@
+# #!/usr/bin/python3
+# from uuid import uuid4
+# from datetime import datetime
+
+# class BaseModel:
+#     """Defines all common attributes/methods for other classes"""
+
+#     def __init__(self, *args, **kwargs):
+#         """Initialize a new BaseModel instance"""
+#         if kwargs:
+#             for key, value in kwargs.items():
+#                 if key == "created_at" or key == "updated_at":
+#                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+#                 if key != "__class__":
+#                     setattr(self, key, value)
+#         else:
+#             self.id = str(uuid4())
+#             self.created_at = self.updated_at = datetime.now()
+
+#     def __str__(self):
+#         """Return a string representation of the BaseModel instance"""
+#         return "[{}] ({}) {}".format(
+#             self.__class__.__name__, self.id, self.__dict__
+#         )
+
+#     def save(self):
+#         """Update updated_at attribute with datetime"""
+#         self.updated_at = datetime.now()
+
+#     def to_dict(self):
+#         """Return a dictionary representation of the BaseModel instance"""
+#         obj_dict = self.__dict__.copy()
+#         obj_dict['__class__'] = self.__class__.__name__
+#         obj_dict['created_at'] = self.created_at.isoformat()
+#         obj_dict['updated_at'] = self.updated_at.isoformat()
+#         return obj_dict
+
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.to_dict()
+        )
+
+    def save(self):
+        """Update updated_at attribute with datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        # Sorting keys as per the specified output format
+        sorted_obj_dict = {k: obj_dict[k] for k in sorted(obj_dict)}
+        return sorted_obj_dict
