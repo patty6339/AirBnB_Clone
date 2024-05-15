38ec216425e75554591527d7fe334949624f1049
Stevecmd <murimi101@gmail.com>
2024-05-15 10:09:58 +0300

    Fixed base model as per requirements

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,19 @@ class BaseModel:
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+
+        dict_repr = {
+            'my_number': obj_dict.get('my_number'),
+            'name': obj_dict.get('name'),
+            'updated_at': repr(obj_dict.get('updated_at')),
+            'id': obj_dict.get('id'),
+            'created_at': repr(obj_dict.get('created_at')),
+            # '__class__': obj_dict.get('__class__')
+        }
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.to_dict()
-        )
+                self.__class__.__name__, self.id, dict_repr)
 
     def save(self):
         """Update updated_at attribute with datetime"""
@@ -34,6 +44,13 @@ class BaseModel:
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        # Sorting keys as per the specified output format
-        sorted_obj_dict = {k: obj_dict[k] for k in sorted(obj_dict)}
+
+        # Ensure the dictionary keys are in the desired order
+        key_order = [
+            'my_number',
+            'name', '__class__',
+            'updated_at', 'id',
+            'created_at'
+        ]
+        sorted_obj_dict = {k: obj_dict[k] for k in key_order if k in obj_dict}
         return sorted_obj_dict
