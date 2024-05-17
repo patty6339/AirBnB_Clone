8ac41a8644ee2a9b0901cd5682ad2bb8c54c7b8a
Stevecmd <murimi101@gmail.com>
2024-05-17 12:10:00 +0300

    More classes created

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contains Review class."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+        Review class that inherits from BaseModel
+        Attributes:
+            place_id (str): The ID of the place
+            user_id (str): The ID of the user
+            text (str): The review text
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module contains State class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        State class that inherits from BaseModel
+            Attributes:
+                name (str): The name of the state, initialized as an empty string
+    """
+    name = ""
