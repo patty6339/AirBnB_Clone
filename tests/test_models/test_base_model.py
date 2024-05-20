b770e3e03be7fadebe67ca073262c9a001e4c5d5
Stevecmd <murimi101@gmail.com>
2024-05-20 09:42:00 +0300

    Added tests for 3. BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+import unittest
+
+class TestBaseModel(unittest.TestCase):
+    def test_save(self):
+        """Test save method"""
+        my_model = BaseModel()
+        initial_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(initial_updated_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        self.assertTrue(isinstance(my_model_dict, dict))
+
+if __name__ == '__main__':
+    unittest.main()
 
 my_model = BaseModel()
 my_model.name = "My First Model"
